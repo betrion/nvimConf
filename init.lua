f0f260d96e9441e6b632b2b88a37bba4768d7e7c
dean cvjetanovic <forteee@gmail.com>
2023-04-14 19:14:41 +0200

    Catppuccin border color overrides

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  -- VSCode extension
+  require("config.lazy")
+else
+  require("config.lazy")
+  -- ordinary Neovim
+  --
+end
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -7,8 +7,11 @@ return {
     return {
       sources = {
         nls.builtins.formatting.prettierd,
+        nls.builtins.formatting.prettier,
         nls.builtins.formatting.stylua,
+
         -- nls.builtins.diagnostics.flake8,
+        --
       },
     }
   end,
